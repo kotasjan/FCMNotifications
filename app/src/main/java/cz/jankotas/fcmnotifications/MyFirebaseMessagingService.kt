7ec84bbbc2f3fe2e1b764acd1207096b44e3ca9e
Jan Kotas <kotasjn@gmail.com>
2023-09-22 15:05:15 +0200

    ✨ Use customized notification

--- app/src/main/java/cz/jankotas/fcmnotifications/MyFirebaseMessagingService.kt
+++ app/src/main/java/cz/jankotas/fcmnotifications/MyFirebaseMessagingService.kt
@@ -1,11 +1,22 @@
 package cz.jankotas.fcmnotifications
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.media.RingtoneManager
+import android.os.Build
 import android.util.Log
+import androidx.core.app.NotificationCompat
 import com.google.firebase.firestore.FieldValue
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
 
 class MyFirebaseMessagingService : FirebaseMessagingService() {
 
@@ -20,7 +31,7 @@ class MyFirebaseMessagingService : FirebaseMessagingService() {
         // Check if message contains a notification payload.
         message.notification?.let {
             Log.d(this::class.simpleName, "Message Notification Body: ${it.body}")
-            // sendNotification(it)
+            sendNotification(it)
         }
     }
 
@@ -44,5 +55,53 @@ class MyFirebaseMessagingService : FirebaseMessagingService() {
             }.addOnFailureListener {
                 Log.d(this::class.simpleName, "Token refresh failed")
             }
+    }
+
+    private fun sendNotification(notification: RemoteMessage.Notification) {
+        val intent = Intent(this, MainActivity::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+        val requestCode = 0
+        val pendingIntent = PendingIntent.getActivity(
+            this,
+            requestCode,
+            intent,
+            PendingIntent.FLAG_IMMUTABLE,
+        )
+
+        val channelId = notification.channelId ?: "fcm_default_channel"
+        val defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
+        val notificationBuilder =
+            NotificationCompat.Builder(this, channelId)
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setContentTitle(notification.title)
+                .setContentText(
+                    notification.body + String.format(
+                        " (%s)",
+                        SimpleDateFormat(
+                            "dd/M/yyyy",
+                            Locale.getDefault(),
+                        ).format(Date()),
+                    ),
+                )
+                .setAutoCancel(true)
+                .setSound(defaultSoundUri)
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setContentIntent(pendingIntent)
+
+        val notificationManager =
+            getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        // Since android Oreo notification channel is needed.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(
+                channelId,
+                "Channel human readable title",
+                NotificationManager.IMPORTANCE_DEFAULT,
+            )
+            notificationManager.createNotificationChannel(channel)
+        }
+
+        val notificationId = 0
+        notificationManager.notify(notificationId, notificationBuilder.build())
     }
 }
