e59379ea5bfe461d4696b892061e598f79f5457d
Jan Kotas <kotasjn@gmail.com>
2023-09-22 14:13:39 +0200

    🔊 Add notification logging

--- app/src/main/java/cz/jankotas/fcmnotifications/MyFirebaseMessagingService.kt
+++ app/src/main/java/cz/jankotas/fcmnotifications/MyFirebaseMessagingService.kt
@@ -5,8 +5,24 @@ import com.google.firebase.firestore.FieldValue
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
 
 class MyFirebaseMessagingService : FirebaseMessagingService() {
+
+    override fun onMessageReceived(message: RemoteMessage) {
+        Log.d(this::class.simpleName, "From: ${message.from}")
+
+        // Check if message contains a data payload.
+        if (message.data.isNotEmpty()) {
+            Log.d(this::class.simpleName, "Message data payload: ${message.data}")
+        }
+
+        // Check if message contains a notification payload.
+        message.notification?.let {
+            Log.d(this::class.simpleName, "Message Notification Body: ${it.body}")
+            // sendNotification(it)
+        }
+    }
 
     override fun onNewToken(token: String) {
         Log.d(this::class.simpleName, "Refreshed token: $token")
